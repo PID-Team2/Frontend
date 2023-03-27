eea0fda0b97e2d3b06c0093394d3ee5502061b63
Yusei-0 <acmnhc@gmail.com>
2023-03-27 21:15:13 +0200

    add: Match regex to SplitText

--- src/app/modules/highlighter/utils/splitText.js
+++ src/app/modules/highlighter/utils/splitText.js
@@ -1,9 +1,29 @@
-export const splitText = (texto) => {
+/**
+ * Esta funcion recibe un { string } y devuelve un arreglo basado
+ * en cortar el string en palabras, conjuntos de espacios con fines de linea y 
+ * otros caracteres independientes
+ * 
+ * @param texto String
+ * 
+ * @example
+ * const res = splitText("const  casa = 'asa';");
+ * console.log(res) // ["const","  ", "casa"," ","="," ","'","asa","'", ";"]
+ */
+const splitText = (texto) => {
 
     if(!texto || typeof texto !== "string")
         return [];
 
-    
+    let  responseData=[];
+    let  regexLetters = /[a-zA-Z]/g; 
 
-    return [];
+    console.log((texto.match(regexLetters)));
+    for(let i=0 ; i < texto.length; i++){
+      console.log(texto[i].match(regexLetters));
+    }
+
+    return responseData;
 }
+
+
+splitText("cas aSd;ca\nad");
